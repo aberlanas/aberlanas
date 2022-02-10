1dde694e0d670e034413d22de4c5e27d5b68a2f9
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2022-02-10 20:13:35 +0100

    Updated

--- R-Learning/R-Sessions/Session-01/Session01.R
+++ R-Learning/R-Sessions/Session-01/Session01.R
@@ -1,6 +1,27 @@
 # Algunos calculos de ejemplo
 
-a <- cos(8)
+#print(paste("El cos(8) es : ",a))
 
-pasteº("Esto es " + a)
+vector = c(1,1,2,3)
 
+media <- mean(vector)
+
+m1 <- matrix(data = vector, nrow = 2, ncol = 2 ,byrow = TRUE)
+
+print(m1)
+
+factor("1","2","3")
+
+numero = 3
+
+### Definicion de funciones 
+
+media <- function(x){
+  valor <- sum(x)/length(x)
+  return(valor)
+}
+
+media(vector)
+media(numero)
+
+###
