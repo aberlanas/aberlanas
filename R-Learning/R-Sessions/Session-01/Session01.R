ff279b9f3046470e458a0dde3e6dda059164c847
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2022-02-10 20:28:36 +0100

    Updated

--- R-Learning/R-Sessions/Session-01/Session01.R
+++ R-Learning/R-Sessions/Session-01/Session01.R
@@ -1,23 +1,19 @@
 # Algunos calculos de ejemplo
 
 #print(paste("El cos(8) es : ",a))
-
 vector = c(1,1,2,3)
-
 media <- mean(vector)
-
 m1 <- matrix(data = vector, nrow = 2, ncol = 2 ,byrow = TRUE)
-
-print(m1)
-
 factor("1","2","3")
-
 numero = 3
 
 ### Definicion de funciones 
 
+vector = c(1,1,2,3)
+numero = 3
+
 media <- function(x){
-  valor <- sum(x)/length(x)
+  valor <- (sum(x)/length(x))
   return(valor)
 }
 
@@ -25,3 +21,9 @@ media(vector)
 media(numero)
 
 ###
+
+poli <- function(x,y,z){
+  return( x^2+y^3+z^4)
+}
+
+print(poli(3,4,5))
